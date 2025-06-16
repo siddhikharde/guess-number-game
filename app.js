4f6150431dbf6ccd19e6d7cf4fb7bfe2e1c6b5b8
siddhikharde <siddhikharde2005@gmail.com>
2025-06-16 09:43:57 +0530

    linked js file to html file

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-const max=prompt("Enter the maximum number:");
-const randomNumber= Math.floor(Math.random()*max)+1;
-const guess = prompt("Guess a number between 1 and " +max + ":");
-function checkGuess(guess, randomNumber) {
-    if (guess < 1 || guess > max) {
-        return "Invalid guess. Please enter a number between 1 and " + max + ".";
-    }
-    else if (guess < randomNumber) {
-        return "Hint : number is too small..";
-    }
-    else if (guess > randomNumber) {
-        return "Hint : number is too large..";
-    }
-    else {
-        return "Congratulations! you guessed the number!";
+
+function checkGuess() {
+    const max=parseInt(prompt("Enter the maximum number:"));
+    const randomNumber= Math.floor(Math.random()*max)+1;
+    let guess = parseInt(prompt("Guess a number between 1 and " +max + ":"));
+    
+    while (true) {
+
+      if (guess === null || guess == "quit"){
+        document.getElementById("result").innerText="Game cancelled.";
+        break;
+      }
+
+      if (isNaN(guess) || guess < 1 || guess > max) {
+        document.getElementById("result").innerText="Invalid input. Please enter a number between 1 and " + max + ".";
+        guess = parseInt(prompt("Guess a number between 1 and " + max + ":"));
+      }
+      else if (guess == randomNumber) {
+         document.getElementById("result").innerText="Congratulations! You guessed the number " + randomNumber + " correctly!";
+        break;
+      }
+      else if (guess > randomNumber) {
+        guess= prompt("Your guess was too high. Try again.");
+        
+      }
+      else {
+      guess =prompt("Your guess was too low. Try again.");
+      }
     }
 }
